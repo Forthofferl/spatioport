e6985e5623398f7f2ca101ade5a20db1f31a8c6c
maraisP <pierre.marais@hotmail.fr>
2015-01-14 23:31:16 +0100

    fix des liens du menu

--- view/menu/viewMenuNonConnecte.php
+++ view/menu/viewMenuNonConnecte.php
@@ -1,17 +1,8 @@
-<li class="active"><a href="index.php?action=classement"><span class="fa fa-sort-amount-asc"></span> Accueil</a></li>
-<li class="active"><a href="index.php?action=statistiques"><span class="fa fa-pie-chart"></span> Recherche de vaisseau</a></li>
-<li class="active"><a href="index.php?action=regles"><span class="fa fa-file-text"></span>Panier </a></li>
-<li class="active"><a href="joueur.php?action=inscription"><span class="fa fa-coffee"></span> S'inscrire</a></li>
-<li class="dropdown"> <a class="dropdown-toggle" href="#" data-toggle="dropdown"><span class="fa fa-toggle-on"></span> Se connecter</a>
-  <div class="dropdown-menu" style="padding: 15px;">
-    <form method="post" action="joueur.php?action=connect">
-      <input type="text" id="id_pseudo" class="form-control" name="pseudo" placeholder="Pseudo" required/><br/>
-      <input type="password" id="id_pwd" class="form-control" name="pwd" placeholder="Mot de passe" required/><br/>
-      <input type="hidden" name="redirurl" value="<?php if(isset($_SERVER['HTTP_REFERER'])) echo $_SERVER['HTTP_REFERER']; ?>" />
-      <input type="submit" class="btn btn-default" name="submit" value="&#xf205; Connexion"/><br/><br/>
-      <a href="joueur.php?action=recoverypwd"><small><span class="fa fa-question-circle"></span> Mot de passe oublié ?</small></a><br/>
-    </form>
-  </div>
+<li class="active"><a href="index.php?"><span class="fa fa-sort-amount-asc"></span> Accueil</a></li>
+<li class="active"><a href="index.php?find"><span class="fa fa-pie-chart"></span> Recherche de vaisseau</a></li>
+<li class="active"><a href="index.php?action=panier"><span class="fa fa-file-text"></span>Panier </a></li>
+<li class="active"><a href="index.php?action=inscription"><span class="fa fa-coffee"></span> S'inscrire</a></li>
+<li class="active"><a href="index.php?action=connect"><span class="fa fa-coffee"></span> Se connecter</a></li>
 </li>
 
   <script type="text/javascript"> // évite que les input sortent du form s'ils sont trop grand
