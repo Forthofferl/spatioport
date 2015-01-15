00840b3d4bff66308b331bc1e16b361be26a0bac
maraisP <pierre.marais@hotmail.fr>
2015-01-15 12:40:39 +0100

    commentaire

--- view/menu/viewMenuConnecte.php
+++ view/menu/viewMenuConnecte.php
@@ -2,4 +2,4 @@
 <li class="active" ><a href="utilisateur.php?action=create"><span class="fa fa-sort-amount-asc"></span> Recherche de vaisseau</a></li>
 <li class="active" ><a href="utilisateur.php?action=panier"><span class="fa fa-pie-chart"></span> Panier</a></li>
 <li class="active"><a href="utilisateur.php?action=profil"><span class="fa fa-user"></span> Profil</a></li>
-<li><a href="utilisateur.php?action=deconnexion"><span class="fa fa-toggle-off"></span> Se déconnecter (<?php echo $_SESSION['pseudo']; ?>)</a></li>
+<li><a href="utilisateur.php?action=deconnexion"><span class="fa fa-toggle-off"></span> Se déconnecter <!--(<?php echo $_SESSION['pseudo']; ?>)--></a></li>
