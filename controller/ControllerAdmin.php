873a4b390b5a9562edba41e709726087c878e850
forthofferl <fortho30@live.fr>
2015-01-15 22:56:04 +0100

    Création d'autre view admin et modification view error rajout d'une variable messageErreur

--- controller/ControllerAdmin.php
+++ controller/ControllerAdmin.php
@@ -21,6 +21,20 @@ switch ($action) {
 		$pagetitle="Erreur!";	
 		}
 		break;
+	
+	case "search":
+	
+		if(estConnecte()&&estAdmin()){
+		$view="Recherche";
+		$pagetitle="Gestion d'un utilisateur";
+		$pseudo="";
+		}
+		else{
+		$view="error";
+		$pagetitle="Erreur!";
+		$messageErreur="Erreur lors du chargement de la page";
+		}
+	break;
 		
 }
 require VIEW_PATH . "view.php";
