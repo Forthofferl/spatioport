798f5a0f1971c72a3789cf5677d916087366e511
maraisP <pierre.marais@hotmail.fr>
2015-01-16 12:43:39 +0100

    commencement recherche vaisseau

---
+++ view/vaisseau/viewSearchVaisseau.php
@@ -0,0 +1,8 @@
+<div class="input-group"><span class="input-group-addon"><i class=""></i></span><input  class="form-control" placeholder="Nom du vaisseau" name="nomVaisseau" id="nomVaisseau" required/></div><br/>
+<div class="input-group"><span class="input-group-addon"><i class="fa fa-calculator"></i></span><input type="number" class="form-control" placeholder="Prix" name="prix" id="prix" min="0" max="999999999" required/></div><br/>
+
+
+
+
+
+  <input type="submit" class="btn btn-default" value="&#xf021; Rechercher" />
